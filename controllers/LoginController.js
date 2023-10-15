75d0b003fb33d9be8ccecdfc334eb3c50fd3527f
Daniel-21MM <danielsanz8001@gmail.com>
2023-10-15 09:26:28 -0600

    Se agregaron alertas cuando hay errores en la base de datos

--- controllers/LoginController.js
+++ controllers/LoginController.js
@@ -1,32 +1,31 @@
-const db = require('../database/db'); // Importamos la conexión a la base de datos desde db.js
-const Swal = require('sweetalert2'); // Asegúrate de importar Sweet Alert en tu archivo si aún no lo has hecho
+const db = require('../database/db');
+const Swal = require('sweetalert2');
 
 function loginUsuario(usuario, contrasena) {
   return new Promise((resolve, reject) => {
-    // Realizamos la consulta SQL para seleccionar un registro de la tabla usuarios que coincida con el usuario y la contraseña proporcionados
     const sql = 'SELECT * FROM usuarios WHERE usuario = ? AND contrasena = ?';
 
     db.connection.query(sql, [usuario, contrasena], (queryErr, results) => {
       if (queryErr) {
         console.error('Error al realizar la consulta: ' + queryErr.message);
+        // Muestra una alerta SweetAlert cuando ocurre un error en la consulta
+        Swal.fire({
+          icon: 'error',
+          title: '¡Oh, no!',
+          text: 'Se ha producido un error al iniciar sesion, inténtalo más tarde.',
+        });
         return reject(queryErr);
       }
 
       if (results.length === 1) {
-        // Si se encuentra un registro que coincide, el inicio de sesión es exitoso
         const usuarioEncontrado = results[0];
-        
-        // Devuelve el objeto de usuario encontrado, incluyendo el rol
         resolve(usuarioEncontrado);
       } else {
-        // Si no se encuentra ningún registro, el inicio de sesión falla
         Swal.fire({
           icon: 'error',
           title: '¡Datos incorrectos!',
           text: 'Verifica tus credenciales',
         });
-
-        // También puedes lanzar una excepción personalizada
         reject(new Error('Credenciales incorrectas'));
       }
     });
