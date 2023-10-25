19612d834d30ea84e8f053495ac06ee9d75dbb4c
Daniel-21MM <danielsanz8001@gmail.com>
2023-10-25 09:40:25 -0600

    se agrego la funcionalidad de eliminar usuarios

--- controllers/showUserController.js
+++ controllers/showUserController.js
@@ -1,65 +1,36 @@
 const db = require('../database/db');
-const Swal = require('sweetalert2');
 
-function formatearTelefono(telefono) {
-    // Convierte el número de teléfono a una cadena.
-    const telefonoStr = telefono.toString();
+function obtenerDatosUsuariosDesdeBD(consultaSQL) {
+    return new Promise((resolve, reject) => {
+        db.connection.query(consultaSQL, (err, results) => {
+            if (err) {
+                console.error('Error al obtener datos de usuarios: ' + err.message);
+                reject(err);
+                return;
+            }
 
-    // Divide el número en grupos de tres dígitos y luego los une con guiones.
-    return telefonoStr.replace(/(\d{3})(\d{3})(\d{2})(\d{2})/, '$1-$2-$3-$4');
+            resolve(results);
+        });
+    });
 }
 
+function eliminarUsuario(idUsuario) {
+    return new Promise((resolve, reject) => {
+        const consultaSQL = 'DELETE FROM usuarios WHERE id = ?';
 
-// Función para cargar los datos de la tabla desde la base de datos.
-async function cargarDatosTabla() {
-    try {
-        const tabla = document.querySelector('#miTabla tbody');
-        tabla.innerHTML = ''; // Limpia el contenido anterior de la tabla.
+        db.connection.query(consultaSQL, [idUsuario], (err) => {
+            if (err) {
+                console.error('Error al eliminar el usuario: ' + err.message);
+                reject(err);
+                return;
+            }
 
-        const consultaSQL = 'SELECT id, usuario, rol, correo, telefono, fecha, imgPerfilUrl FROM usuarios';
-
-        const resultados = await obtenerDatosUsuariosDesdeBD(consultaSQL);
-
-        resultados.forEach((usuario) => {
-            // Formatea la fecha al estilo "yyyy-mm-dd"
-            const fecha = new Date(usuario.fecha).toISOString().split('T')[0];
-
-            const rolTexto = usuario.rol === 1 ? 'Administrador' : 'Empleado';
-
-            // Formatea el número de teléfono
-            const telefonoFormateado = formatearTelefono(usuario.telefono);
-
-            const tr = document.createElement('tr');
-            tr.innerHTML = `
-                <td>${usuario.id}</td>
-                <td><img src="${usuario.imgPerfilUrl}" alt="Perfil de usuario"></td>
-                <td>${usuario.usuario}</td>
-                <td>${rolTexto}</td>
-                <td>${usuario.correo}</td>
-                <td>${telefonoFormateado}</td>
-                <td>${fecha}</td>
-                <td>
-                    <button><i class='bx bx-edit'></i></button>
-                    <button class="delete"><i class='bx bx-trash'></i></button>
-                </td>
-            `;
-
-            tabla.appendChild(tr);
+            resolve();
         });
-
-    } catch (error) {
-        console.error('Error al cargar los datos en la tabla: ', error);
-        Swal.fire({
-            icon: 'error',
-            title: 'Error al cargar los datos',
-            text: 'No se pudieron cargar los datos de la base de datos.',
-        });
-    }
+    });
 }
 
-// Llama a la función para cargar los datos de la tabla cuando se carga la página.
-document.addEventListener('DOMContentLoaded', () => {
-    cargarDatosTabla();
-});
-
-// ... (otras partes de tu código)
+module.exports = {
+    obtenerDatosUsuariosDesdeBD,
+    eliminarUsuario,
+};
--- models/showUsersModel.js
+++ models/showUsersModel.js
@@ -1,18 +1,87 @@
-// Función para obtener datos de usuarios desde la base de datos.
-function obtenerDatosUsuariosDesdeBD(consultaSQL) {
-    return new Promise((resolve, reject) => {
-        db.connection.query(consultaSQL, (err, results) => {
-            if (err) {
-                console.error('Error al obtener datos de usuarios: ' + err.message);
-                reject(err);
-                return;
-            }
+const db = require('../database/db');
+const Swal = require('sweetalert2');
+const modelo = require('../controllers/showUserController');
 
-            resolve(results);
-        });
-    });
+function formatearTelefono(telefono) {
+    const telefonoStr = telefono.toString();
+    return telefonoStr.replace(/(\d{3})(\d{3})(\d{2})(\d{2})/, '$1-$2-$3-$4');
 }
 
-module.exports = {
-    obtenerDatosUsuariosDesdeBD,
-};
+async function cargarDatosTabla() {
+    try {
+        const tabla = document.querySelector('#miTabla tbody');
+        tabla.innerHTML = '';
+
+        const consultaSQL = 'SELECT id, usuario, rol, correo, telefono, fecha, imgPerfilUrl FROM usuarios';
+
+        const resultados = await modelo.obtenerDatosUsuariosDesdeBD(consultaSQL);
+
+        resultados.forEach((usuario) => {
+            const fecha = new Date(usuario.fecha).toISOString().split('T')[0];
+            const rolTexto = usuario.rol === 1 ? 'Administrador' : 'Empleado';
+            const telefonoFormateado = formatearTelefono(usuario.telefono);
+
+            const tr = document.createElement('tr');
+            tr.innerHTML = `
+                <td>${usuario.id}</td>
+                <td><img src="${usuario.imgPerfilUrl}" alt="Perfil de usuario"></td>
+                <td>${usuario.usuario}</td>
+                <td>${rolTexto}</td>
+                <td>${usuario.correo}</td>
+                <td>${telefonoFormateado}</td>
+                <td>${fecha}</td>
+                <td>
+                    <button><i class='bx bx-edit'></i></button>
+                    <button class="delete" data-id="${usuario.id}"><i class='bx bx-trash'></i></button>
+                </td>
+            `;
+
+            tabla.appendChild(tr);
+
+            const deleteButton = tr.querySelector('.delete');
+            deleteButton.addEventListener('click', eliminarUsuario);
+        });
+    } catch (error) {
+        console.error('Error al cargar los datos en la tabla: ', error);
+        Swal.fire({
+            icon: 'error',
+            title: 'Error al cargar los datos',
+            text: 'No se pudieron cargar los datos de la base de datos.',
+        });
+    }
+}
+
+function eliminarUsuario(event) {
+    const row = event.currentTarget.parentElement.parentElement;
+    if (row) {
+        const idUsuario = event.currentTarget.getAttribute('data-id');
+
+        Swal.fire({
+            title: '¿Estás seguro?',
+            text: 'Esta acción eliminará el registro de usuario.',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Sí, eliminar',
+            cancelButtonText: 'Cancelar'
+        }).then(async (result) => {
+            if (result.isConfirmed) {
+                try {
+                    await modelo.eliminarUsuario(idUsuario);
+                    row.remove();
+                    Swal.fire('Eliminado', 'El registro ha sido eliminado.', 'success');
+                } catch (error) {
+                    console.error('Error al eliminar el usuario: ', error);
+                    Swal.fire('Error', 'No se pudo eliminar el registro de usuario.', 'error');
+                }
+            }
+        });
+    } else {
+        console.error('Error: No se pudo encontrar el elemento padre.');
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    cargarDatosTabla();
+});
